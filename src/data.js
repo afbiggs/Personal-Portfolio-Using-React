9df09c998ee96242569d2079134fb3d1f4303782
afbiggs <112245251+afbiggs@users.noreply.github.com>
2023-03-14 02:19:24 -0400

    added new layout and functionality of portfolio page

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+export const projects = [
+    {
+      title: "ReadMe Generator",
+      subtitle: "JavaScript, Inquirer and Node.js",
+      description:
+        "A CLI professional ReadMe file generator. A user will be prompted to answer a series of question and upon completion a README.md file will be generated.",
+      image: require("./assets/readme-generator-scaled.jpg"),
+      link: "https://github.com/afbiggs/Readme-Generator",
+    },
+    {
+      title: "Work-Day Schedular App",
+      subtitle: "HTML, CSS, jQuery",
+      description:
+        "A functioning calendar application that allows a user to save events for each hour of the day. It runs in the browser and components dynamically update based on user input or time of day.",
+      image: require("./assets/workdayScheduler.jpeg"),
+      link: "https://github.com/afbiggs/bookish-octo-funicular",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
